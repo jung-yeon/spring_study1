5aede6f38b20e7cbd8cea54a2adc582207d64746
jung-yeon <99dlwjddus@naver.com>
2022-10-11 10:39:36 +0900

    first commit

---
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/User.java
@@ -0,0 +1,17 @@
+package com.nhnacademy.edu.springframework;
+
+public class User {
+    private final String email;
+    private final String phoneNumber;
+
+    public User(String email, String phoneNumber){
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+    public String getEmail(){
+        return this.email;
+    }
+    public String getPhoneNumber(){
+        return this.phoneNumber;
+    }
+}
